5ccece59657765f95f6a98da0953b2146f35e793
Adam46072 <adam.bartl2001@seznam.cz>
2023-10-05 11:36:20 +0200

    Connection system with login student or teacher

--- InformationSystem/Login.cs
+++ InformationSystem/Login.cs
@@ -8,5 +8,23 @@ namespace InformationSystem
 {
     internal class Login
     {
+        public object LoginIntoSystem(string username, string password, List<Student>? students, List<Teacher>? teachers)
+        {
+            foreach (Student student in students)
+            {
+                if(student.username.Equals(username) && student.password.Equals(password))
+                {
+                    return student;
+                }
+            }
+            foreach (Teacher teacher in teachers)
+            {
+                if (teacher.username.Equals(username) && teacher.password.Equals(password))
+                {
+                    return teacher;
+                }
+            }
+            return null;
+        }
     }
 }
--- InformationSystem/System.cs
+++ InformationSystem/System.cs
@@ -11,16 +11,32 @@ namespace InformationSystem
         public List<Student> students { get; private set; }
         public List<Teacher> teachers { get; private set; }
         private Administrator admin { get; init; }
+        private Login login { get; init; }
         public System(Administrator admin, Login login)
         {
             students = new List<Student>();
             teachers = new List<Teacher>();
+            this.login = login;
             this.admin = admin;
         }
-        public void LoginIntoSystem()
+        public void LoginIntoSystem(string username, string password)
         {
-
+            object signIn = login.LoginIntoSystem(username, password, students, teachers);
+            if(signIn is Student)
+            {
+                Console.WriteLine(@"You can choose between:
+1 - Schedule 
+2 - School grades
+3 - Homework");
             manipulationSystem();
+            }
+            if(signIn is Teacher)
+            {
+                Console.WriteLine(@"You can choose between:
+1 - Schedule 
+2 - Students school grades
+3 - new homework");
+            }
         }
         private void manipulationSystem()
         {
